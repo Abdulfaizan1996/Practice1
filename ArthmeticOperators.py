d57e038d77fea85adf47bffb9d0ac4c6ebd6fed2
abdul.faizan <abdul.faizan@globallogic.com>
2024-02-18 12:28:49 +0530

    Arthmetic operators

---
+++ ArthmeticOperators.py
@@ -0,0 +1,17 @@
+a = 9
+b = 4
+add = a + b
+sub = a - b
+mul = a * b
+div = a / b
+floordiv = a / b
+mod = a % b
+pow = a**b
+
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(floordiv)
+print(mod)
+print(pow)
